ee7ecb010dda34b37f488a24911c4bf1d04d8c5d
Davyd Holovii <davyd.holovii@ext.chargepoint.com>
2023-05-25 10:35:18 -0400

    mob-407 extra outputs

--- scripts/mob407.php
+++ scripts/mob407.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/bootstrap.php';
 
 connect();
 
-$analyzer = new \App\Mob407\FixAnalyzer(dirname(__DIR__));
+$analyzer = new \App\Mob407\FixAnalyzer(rootPath: dirname(__DIR__));
 $analyzer->run([
 //    'force_recreate_users_table',
 //    'force_recreate_payments_history_table',
