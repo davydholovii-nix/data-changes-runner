030ec4ece68ab83b7936b6e516ccc7dce5760542
Davyd Holovii <davyd.holovii@ext.chargepoint.com>
2023-05-26 11:02:36 -0400

    mob-407 existing report parser

---
+++ scripts/mob407_report_parser.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+$sourcesDir = dirname(__DIR__) . '/sources/mob407_eu';
+$outputDir = dirname(__DIR__) . '/var/mob407_eu/parsed_report';
+
+if (!file_exists($outputDir)) {
+    mkdir(directory: $outputDir, recursive: true);
+}
+
+$reportParser = new \App\Mob407\ReportParser(
+    sourcesDir: $sourcesDir,
+    outputDir: $outputDir,
+);
+$reportParser->parse([
+    'mob407_group2.csv',
+    'mob407_group3.csv',
+]);
