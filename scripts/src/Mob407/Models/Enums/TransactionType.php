02eddbb5a44f4bb78e52f432fde10d0abe29e487
Davyd Holovii <davyd.holovii@ext.chargepoint.com>
2023-09-15 16:12:32 -0400

    Remove scripts files

--- scripts/src/Mob407/Models/Enums/TransactionType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Mob407\Models\Enums;
-
-enum TransactionType: string
-{
-    case BUSINESS = 'BUSINESS';
-    case PERSONAL = 'PERSONAL';
-}
--- sql/DB_PREP/create_example_table.sql
+++
@@ -1,6 +0,0 @@
-create table examples (
-    id int primary key auto_increment,
-    name varchar(255) not null,
-    created_at timestamp default current_timestamp
-);
-insert into examples (name) values ('example 1');
